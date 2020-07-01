bca92221a7a98d9114a7175a4d7b017171c7c06c
ROBERT ANGEL TORRES <rtorres61595@gmail.com>
2020-06-30 21:43:43 -0400

    blah

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
     connection = mysql.createConnection({
-    host: "localhost",
+    host: "k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
     port: 3306,
-    user: "root",
-    password: "root",
+    user: "hsryuxpnt29rgn3u",
+    password: "z009061zlsdrzmh6",
     database: "burgers_db",
   });
   }
